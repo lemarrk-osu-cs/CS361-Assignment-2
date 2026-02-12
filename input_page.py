5b62cbe61180bf55475ea8005dd59b2f8d492a79
Kendall Lemarr <lemarrk@oregonstate.edu>
2026-02-11 17:51:52 -0800

    centered all buttons and elements in card

--- input_page.py
+++ input_page.py
@@ -2,12 +2,14 @@ from nicegui import ui
 
 
 with ui.column().classes('fixed-center'):
-    ui.label('Budget Input')
-    with ui.card().classes('flex-grow'):
-        ui.input( placeholder='Input Monthly Amount',on_change=lambda e: result.set_text('you clicked!' + e.value),
+    with ui.column().classes('w-full items-center'):
+        ui.label('Budget Input')
+        with ui.card().classes('flex-grow').classes('w-full items-center'):
+            ui.input( placeholder='Input Monthly Amount',on_change=lambda e: result.set_text('you clicked!' + e.value),
                 validation={'Input must be greater than zero ': lambda value: len(value) > 0})
-        ui.input( placeholder='Input Weekly Amount',on_change=lambda e: result.set_text('you clicked!' + e.value),
+            ui.input( placeholder='Input Weekly Amount',on_change=lambda e: result.set_text('you clicked!' + e.value),
                 validation={'Input must be greater than zero ': lambda value: len(value) > 0})
-        ui.button('Continue')
+            ui.button('Continue')   
+            ui.button("Dark Mode",on_click=lambda:ui.notify('dark mode engaged'))
 
 ui.run(title="Input Page")
