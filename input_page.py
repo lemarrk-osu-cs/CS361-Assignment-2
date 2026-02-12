f42f6a08ef3bd452bb58e36b35a7cccc99dfc06e
Kendall Lemarr <lemarrk@oregonstate.edu>
2026-02-11 21:28:02 -0800

    added opacity to onclick event on error  messages

--- input_page.py
+++ input_page.py
@@ -1,7 +1,8 @@
 from nicegui import ui
 
+
 def on_continue_press(event):
-    ui.notify(f"""Continue was pressed: {event}""")
+    ui.notify(f"Continue was pressed:")
 
 def on_darkmode_pressed():
     ui.notify('dark mode engaged')
@@ -10,23 +11,21 @@ def on_input_validation(value):
     try:
         conv = int(value) 
     except ValueError as e:
-        ui.label(f'Input must a number {e}')
+        err1 = ui.label(f'Input must a number {e}').on("click",lambda:err1.classes('opacity-0'))
     if conv <= 0:
-        ui.label(f'Input must a number greater than 0')
+        err2 = ui.label(f'Input must a number greater than 0').on("click",lambda:err2.classes('opacity-0'))
 
-with ui.column().classes('fixed-center max-w-3xl'):
-    with ui.column().classes('w-full items-center'):
-        ui.label('Budget Input')
-        with ui.card().classes('flex-grow').classes('w-full items-center'):
-            m_amount = ui.input(placeholder='Input Monthly Amount',
-                                on_change=lambda e: e.set_text('You entered!' + e.value),
-                validation=on_input_validation).props('clearable').without_auto_validation()
-        
-            w_amount = ui.input(placeholder='Input Weekly Amount', 
-                                 on_change=lambda e: e.set_text('You entered!' + e.value),
-                validation=on_input_validation).props('clearable').without_auto_validation()
-            
-            cont_click = ui.button('Continue',on_click=on_continue_press)   
-            d_mode = ui.button("Dark Mode",on_click=on_darkmode_pressed)
+with ui.row().classes('h-screen w-full items-center justify-center'):
+        with ui.column(align_items='center').classes('w-120 items-center justify-center'):
+             ui.label('Budget Input')
+             with ui.card().classes('flex-grow').classes('w-full items-center'):
+                ui.input(placeholder='Input Monthly Amount', on_change=lambda e: result.set_text('You entered!' + e.value),
+                     validation=on_input_validation).props('clearable').classes('w-90')
+                ui.input(placeholder='Input Weekly Amount', on_change=lambda e: result.set_text('You entered!' + e.value),
+                        validation=on_input_validation).props('clearable').classes('w-90')
+                              
+                ui.button('Continue',on_click=on_continue_press).classes('w-full')  
+                ui.button("Dark Mode",on_click=on_darkmode_pressed).classes('w-full')    
+
 
 ui.run(title="Input Page")
