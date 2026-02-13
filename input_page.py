45af06675372eec2560786599e0f039e91b3fda2
Kendall Lemarr <lemarrk@oregonstate.edu>
2026-02-12 21:30:05 -0800

    added help button and help message

--- input_page.py
+++ input_page.py
@@ -1,11 +1,11 @@
 from nicegui import ui
 
 
-def on_continue_press(event):
-    ui.notify(f"Continue was pressed:")
+def on_continue_pressed(event):
+    ui.notify(f"Continue was pressed")
 
 def on_darkmode_pressed():
-    ui.notify('dark mode engaged')
+    ui.notify('Dark mode engaged')
 
 def on_input_monthly_validation(value):
     conv:int
@@ -34,17 +34,22 @@ def on_change_input_one(e):
 def on_change_input_two(e):
      label_two.set_text(f'You entered!{e.value}')
 
+def on_click_help_button():
+    ui.notify("""Hello and Thank You for using this app. Please input your monthly amount of funds to spend minus your 
+              known monthly expenditures. By continuing to use the features of this page you can easily input all 
+              information needed to set your monthly, weekly and daily budget""")
 
 with ui.row().classes('h-screen w-full items-center justify-center'):
         with ui.column(align_items='center').classes('w-120 items-center justify-center'):
-             ui.label('Budget Input')
+             ui.label('Budget Input').classes('w-120 text-center p5')
+             ui.button('Help?',color='red', on_click=on_click_help_button).classes('text-white w-120')
              with ui.card().classes('flex-grow').classes('w-full items-center'):
                 label_one = ui.input(placeholder='Input Monthly Amount', on_change=on_change_input_one,
                      validation=on_input_monthly_validation).props('clearable').classes('w-90')
                 label_two = ui.input(placeholder='Input Weekly Amount', on_change=on_change_input_two,
                      validation=on_input_weekly_validation).props('clearable').classes('w-90')
                               
-                ui.button('Continue',on_click=on_continue_press).classes('w-full')  
+                ui.button('Continue',on_click=on_continue_pressed).classes('w-full')  
                 ui.button("Dark Mode",on_click=on_darkmode_pressed).classes('w-full')    
 
 
