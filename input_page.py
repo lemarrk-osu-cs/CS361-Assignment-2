924d1093f9bf0136c7c7c00b154ed2cf0cc697dc
Kendall Lemarr <lemarrk@oregonstate.edu>
2026-02-13 17:14:11 -0800

    added changes to help? message and account summary help message, with odds n ends mixed in

--- input_page.py
+++ input_page.py
@@ -18,7 +18,7 @@ def on_input_monthly_validation(value):
     if conv <= 0:
         err2 = ui.label(f'Monthly input must a number greater than 0').on("click",lambda:on_click_delete_label(err2)).style('font-size:18px')
 
-def on_input_weekly_validation(value):
+def on_input_monthly_expenses_validation(value):
     conv:int
     try:
         conv = int(value) 
@@ -34,16 +34,18 @@ def on_change_input_two(e):
      app.storage.user['val1'] = e.value
 
 def on_click_input_help_button():
-    ui.notify("""Hello and Thank You for using this app. Please input your monthly amount of funds to spend minus your 
-              known monthly expenditures. By continuing to use the features of this page you can easily input all 
-              information needed to set your monthly, weekly and daily budget!""").style('font-size:18px') 
+    ui.timer(20.0, ui.notify("""Hello and Thank You for using this app. Please input your monthly expenditures of and your 
+              known monthly income. By continuing to use the features of this page you can easily input all 
+              information needed to set your monthly, weekly and daily budget! Just press "Continue" or the "Arrow Keys"
+              when you are done to be able to calculate your budget for Weekly and Daily amounts.
+              """),once=True).style('font-size:18px')
 
 def on_click_account_help_button():
-    ui.notify("""The input of your monthly and weekly information needed to set your daily budget
-              is here. The daily allowance is calculated by the days left divided into the monthly amount.
+    ui.timer(20.0, ui.notify("""Your monthly and weekly information needed to set your daily budget
+              is here. The daily allowance is calculated by the number of days in the month divided into the monthly amount.
               The weekly amount is very similarly calculated as the number of weeks divided into the monthly
-               amount of money inputed.
-             """).style('font-size:18px') 
+               amount of income given.
+             """),once=True).style('font-size:18px')
 
 def on_click_navigate_to_summary():
     ui.navigate.to('/account_summary')
@@ -59,10 +61,10 @@ def input_page():
                 ui.label('Budget Input').classes('w-120 text-center').style('font-size:30px').style('font-size:18px')
                 ui.button('Help?',color='red', on_click=on_click_input_help_button).classes('text-white w-120').style('font-size:18px')
                 with ui.card().classes('flex-grow').classes('w-full items-center'):
-                    ui.input(placeholder='Input Monthly Amount', on_change=on_change_input_one,
+                    ui.input(placeholder='Input Monthly Income', on_change=on_change_input_one,
                         validation=on_input_monthly_validation).props('clearable').classes('w-90').style('font-size:18px')
-                    ui.input(placeholder='Input Weekly Amount', on_change=on_change_input_two,
-                        validation=on_input_weekly_validation).props('clearable').classes('w-90').style('font-size:18px')             
+                    ui.input(placeholder='Input Monthly Expenses', on_change=on_change_input_two,
+                        validation=on_input_monthly_expenses_validation).props('clearable').classes('w-90').style('font-size:18px')             
                     ui.button('Continue',on_click=on_click_continue_pressed).classes('w-full').style('font-size:18px') 
                     ui.button("Dark Mode",on_click=make_dark).classes('w-full').style('font-size:18px') 
             ui.button('->',on_click=on_click_navigate_to_summary).style('font-size:18px')
